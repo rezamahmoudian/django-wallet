87a5ec79b3b402da3b8e3d681b37ae3f2784459c
Reza Mahmoudian <rezamahmoudian80@gmail.com>
2023-07-30 13:23:08 +0330

    create shaba serializer

--- wallet/serializers.py
+++ wallet/serializers.py
@@ -15,3 +15,18 @@ class TransactionSerializer(serializers.ModelSerializer):
 		model = Transaction
 		fields = "__all__"
 
+
+class ShabaSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Shaba
+		fields = ['shaba_number', 'bank_name', 'full_name']
+
+	def create(self, validated_data):
+		request = self.context.get("request")
+		active_link = get_random_secret_key()
+		wallet_id = request.user.wallet.id
+		wallet = Wallet.objects.get(id=wallet_id)
+		print("validation data: ")
+		print(validated_data)
+		shaba = Shaba.objects.create(active_link=active_link, wallet=wallet, **validated_data)
+		return shaba
